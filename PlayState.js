79843547f7b0f6c5adf6535bdea7a17adf3449ec
Fernando Bevilacqua <dovyski@gmail.com>
2015-12-12 18:59:12 +0100

    Get the party started!

---
+++ PlayState.js
@@ -0,0 +1,17 @@
+/**
+ * Describes the play state.
+ */
+
+var PlayState = function() {
+	var mZombies;
+
+	this.create = function() {
+		var i;
+
+		mZombies = this.game.add.group();
+	};
+
+	this.update = function() {
+
+	};
+};
